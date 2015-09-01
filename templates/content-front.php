331a50dbb92474fad7d40aa9e83862ae40376153
s10n <a@akaiv.com>
2015-09-02 03:23:28 +0900

    첫 화면에 슬러그가 'home'인 포스트 하나의 본문을 출력

--- templates/content-front.php
+++ templates/content-front.php
@@ -1,5 +1,21 @@
+<?php akaiv_before_post(); ?>
+
+<?php
+  $posts = get_posts( array (
+    'name' => 'home',
+    'post_type' => 'page',
+    'post_status' => 'publish',
+    'posts_per_page' => 1
+  ) );
+  the_post();
+?>
+
 <div class="container">
   <div class="page-content">
-    <p>Welcome!</p>
+    <?php the_content(); ?>
   </div>
 </div>
+
+<?php akaiv_after_post(); ?>
+
+<!-- 여기부터 구인 목록 추가 -->
